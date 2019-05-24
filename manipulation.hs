0135b7b1e05ecb738fef585674047f91ed217040
Kate <BalintKE20@pwcs-edu.org>
2019-05-24 09:15:28 -0400

    added trial method

--- manipulation.hs
+++ manipulation.hs
@@ -5,3 +5,8 @@ reverseIfOdd text num = if ((num `rem` 2) == 0) then text else reverse text
 boustrophedon :: [String] -> [String]
 boustrophedon [] = []
 boustrophedon (x:xs) = x : (reverse (head xs)) : boustrophedon (tail xs)
+
+trial :: String -> [String]
+trial [] = []
+trial text = if(length text < 20) then [text] 
+             else [take 20 text] ++ trial (drop 20 text)
